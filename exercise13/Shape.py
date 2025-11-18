13860ec7a3dfc77f702a4e3509f6df887f8688b0
merazkuet36 <merazkuet36@gmail.com>
2025-11-18 08:20:19 +0000

    adding 13

--- exercise13/Shape.py
+++ exercise13/Shape.py
@@ -1,24 +1,16 @@
-
-
-
 class Shape:
 
-
     def __init__(self):
-
         pass
 
-
+    def __str__(self):
         return "Shape object"
 
     def area(self):
-
         return 0.0
 
     def perimeter(self):
-
         return 0.0
 
     def volume(self):
-
         return 0.0
